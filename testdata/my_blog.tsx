b9a4c5e6c64f3085dbb71c44b736cabd64ed14a9
X <i@jex.me>
2022-06-02 18:41:12 +0800

    Update picture

--- testdata/my_blog.tsx
+++ testdata/my_blog.tsx
@@ -6,7 +6,8 @@ blog({
   author: "Denobot",
   title: "My Blog",
   description: "Description.",
-  picture: "cat.png",
+  picture: "https://deno-avatar.deno.dev/avatar/blog.svg",
+  background: "#f9f9f9",
   links: [
     { title: "Email", url: "mailto:bot@deno.com" },
     { title: "GitHub", url: "https://github.com/denobot" },
