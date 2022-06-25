8d3e0cdf28ef7d4afe02ba7355173cbf282506a6
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-06-25 17:58:33 +0200

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -19,7 +19,7 @@ export {
   h,
   html,
   type VNode,
-} from "https://deno.land/x/htm@0.0.6/mod.tsx";
+} from "https://deno.land/x/htm@0.0.8/mod.tsx";
 export { parse as frontMatter } from "https://deno.land/x/frontmatter@v0.1.4/mod.ts";
 export {
   createReporter,
