dcdd47be0a522a8ca8766f95773a5fa73acb2c02
Roj <ez@roj.im>
2022-06-26 18:24:44 +0300

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -1,17 +1,17 @@
 // Copyright 2022 the Deno authors. All rights reserved. MIT license.
 
-export { serveDir } from "https://deno.land/std@0.140.0/http/file_server.ts";
-export { walk } from "https://deno.land/std@0.140.0/fs/walk.ts";
+export { serveDir } from "https://deno.land/std@0.145.0/http/file_server.ts";
+export { walk } from "https://deno.land/std@0.145.0/fs/walk.ts";
 export {
   dirname,
   fromFileUrl,
   join,
   relative,
-} from "https://deno.land/std@0.140.0/path/mod.ts";
+} from "https://deno.land/std@0.145.0/path/mod.ts";
 export {
   type ConnInfo,
   serve,
-} from "https://deno.land/std@0.140.0/http/mod.ts";
+} from "https://deno.land/std@0.145.0/http/mod.ts";
 
 export * as gfm from "https://deno.land/x/gfm@0.1.20/mod.ts";
 export {
@@ -19,7 +19,7 @@ export {
   h,
   html,
   type VNode,
-} from "https://deno.land/x/htm@0.0.8/mod.tsx";
+} from "https://deno.land/x/htm@0.0.10/mod.tsx";
 export { parse as frontMatter } from "https://deno.land/x/frontmatter@v0.1.4/mod.ts";
 export {
   createReporter,
@@ -30,4 +30,4 @@ export { Feed, type Item as FeedItem } from "https://esm.sh/feed@4.2.2";
 export { default as removeMarkdown } from "https://esm.sh/remove-markdown@0.5.0";
 
 // Add syntax highlighting support for C by default
-import "https://esm.sh/prismjs@1.27.0/components/prism-c?no-check";
+import "https://esm.sh/prismjs@1.28.0/components/prism-c?no-check";
