2651e838d5c1984fd7a894dcb4ac46836c8db737
Roj <ez@roj.im>
2022-06-26 18:21:12 +0300

    fix: add `!important` to remove the background in dark mode

--- blog.tsx
+++ blog.tsx
@@ -324,7 +324,7 @@ export async function handler(
       },
       styles: [
         gfm.CSS,
-        `.markdown-body { --color-canvas-default: transparent; --color-canvas-subtle: #edf0f2; --color-border-muted: rgba(128,128,128,0.2); } .markdown-body img + p { margin-top: 16px; }`,
+        `.markdown-body { --color-canvas-default: transparent !important; --color-canvas-subtle: #edf0f2; --color-border-muted: rgba(128,128,128,0.2); } .markdown-body img + p { margin-top: 16px; }`,
         ...(blogState.style ? [blogState.style] : []),
         ...(post.background ? [`body{background:${post.background};}`] : (
           blogState.background
