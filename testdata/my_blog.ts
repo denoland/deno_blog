5dd5cc21c1e2c82e32bcff27e6c6074594974388
Roj <roj1512@aol.com>
2022-06-27 21:22:43 +0300

    chore: recover my_blog.ts

---
+++ testdata/my_blog.ts
@@ -0,0 +1,15 @@
+import blog from "../blog.tsx";
+
+blog({
+  author: "Dino",
+  title: "My Blog",
+  description: "The blog description.",
+  avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
+  avatarClass: "rounded-full",
+  background: "#f9f9f9",
+  links: [
+    { title: "bot@deno.com", url: "mailto:bot@deno.com" },
+    { title: "GitHub", url: "https://github.com/denobot" },
+    { title: "Twitter", url: "https://twitter.com/denobot" },
+  ],
+});
