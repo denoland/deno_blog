e37cf19ed8cb9919e05054d3bc2859ec6607ee93
Roj <ez@roj.im>
2022-06-30 13:11:41 +0300

    chore: improve punctuation

--- components.tsx
+++ components.tsx
@@ -207,7 +207,8 @@ function Footer(props: { author?: string }) {
     <footer class="mt-20 pb-16 lt-sm:pb-8 lt-sm:mt-16">
       <p class="flex items-center gap-2.5 text-gray-400/800 dark:text-gray-500/800 text-sm">
         <span>
-          &copy; {new Date().getFullYear()} {props.author}, Powered by{" "}
+          &copy; {new Date().getFullYear()} {props.author} &middot; Powered by
+          {" "}
           <a
             class="inline-flex items-center gap-1 underline hover:text-gray-800 dark:hover:text-gray-200 transition-colors"
             href="https://deno.land/x/blog"
