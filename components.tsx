e072d62b055cd2fae3380678afe78f33b728b1e6
Aoiste <8148113+aoiste@users.noreply.github.com>
2022-07-04 12:25:33 +0800

    chore: format code

--- components.tsx
+++ components.tsx
@@ -268,16 +268,20 @@ function PrettyDate({ date, timezone }: { date: Date; timezone?: string }) {
 
 function Tags({ tags }: { tags?: string[] }) {
   return (
-    tags && tags.length > 0 ? <section class="flex gap-x-2 flex-wrap">
-      {tags?.map((tag) => (
-        <a
-          class="text-bluegray-500 font-bold"
-          href={`/?tag=${tag}`}
-        >
-          #{tag}
-        </a>
-      ))}
-    </section> : null
+    tags && tags.length > 0
+      ? (
+        <section class="flex gap-x-2 flex-wrap">
+          {tags?.map((tag) => (
+            <a
+              class="text-bluegray-500 font-bold"
+              href={`/?tag=${tag}`}
+            >
+              #{tag}
+            </a>
+          ))}
+        </section>
+      )
+      : null
   );
 }
 
