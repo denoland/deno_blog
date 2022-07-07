d5aad02d6d26d1d8fcfd6cf5ee86c3d1621fac5d
omar2205 <omar2205a@yahoo.com>
2022-07-07 00:34:00 +0000

    add and use UnoConfig type

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,6 @@
 // Copyright 2022 the Deno authors. All rights reserved. MIT license.
 
-import type { ConnInfo, VNode } from "./deps.ts";
+import type { ConnInfo, UnoConfig, VNode } from "./deps.ts";
 
 export interface BlogContext {
   state: BlogState;
@@ -30,7 +30,7 @@ export interface BlogSettings {
   lang?: string;
   timezone?: string;
   canonicalUrl?: string;
-  unocss?: any; // "object"
+  unocss?: UnoConfig;
 }
 
 export interface BlogState extends BlogSettings {
