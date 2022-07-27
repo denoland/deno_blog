479aad4dd0ea2e7434725265e5b1116056b28611
Roj <ez@roj.im>
2022-07-27 18:57:47 +0300

    docs: describe the properties of `BlogSettings`
    Closes #56.

--- types.d.ts
+++ types.d.ts
@@ -15,33 +15,59 @@ export interface BlogMiddleware {
 export type DateStyle = "full" | "long" | "medium" | "short";
 
 export interface BlogSettings {
+  // The blog title
   title?: string;
+  // The blog description
   description?: string;
+  // URL to avatar. Can be relative.
   avatar?: string;
+  // CSS classes to use with the avatar.
   avatarClass?: string;
+  // URL to background cover. Can be relative.
   cover?: string;
+  // Color of the text that will be on the background cover.
   coverTextColor?: string;
+  // The author of the blog. Can be overridden by respective post settings.
   author?: string;
+  // Social links
   links?: {
+    // The link title
     title: string;
+    // The link
     url: string;
+    // The element to use as the icon of the link
     icon?: VNode;
+    // The link target
     target?: "_self" | "_blank" | "_parent" | "_top";
   }[];
+  // The element ot use as header
   header?: VNode;
+  // Whether to show the header on post pages
   showHeaderOnPostPage?: boolean;
+  // The element to use as section
   section?: VNode;
+  // The element to use as footer
   footer?: VNode;
+  // Custom CSS
   style?: string;
+  // URL to open graph image. Can be relative.
   ogImage?: string;
   middlewares?: BlogMiddleware[];
+  // The ISO code of the language the blog is in
   lang?: string;
+  // Style of 
   dateStyle?: DateStyle;
+  // The canonical URL of the blog
   canonicalUrl?: string;
+  // UnoCSS configuration
   unocss?: UnoConfig;
+  // Color scheme
   theme?: "dark" | "light" | "auto";
+  // URL to favicon. Can be relative.
   favicon?: string;
+  // The port to serve the blog on
   port?: number;
+  // The hostname to serve the blog on
   hostname?: string;
 }
 
