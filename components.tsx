5ab1b5da9367e24c88a8f0a29ad211a5361c0d69
Ryan Dahl <ry@tinyclouds.org>
2022-09-03 15:28:55 -0400

    fmt

--- components.tsx
+++ components.tsx
@@ -231,8 +231,7 @@ function Footer(props: { author?: string }) {
     <footer class="mt-20 pb-16 lt-sm:pb-8 lt-sm:mt-16">
       <p class="flex items-center gap-2.5 text-gray-400/800 dark:text-gray-500/800 text-sm">
         <span>
-          Powered by
-          {" "}
+          Powered by{" "}
           <a
             class="inline-flex items-center gap-1 underline hover:text-gray-800 dark:hover:text-gray-200 transition-colors"
             href="https://deno.land/x/blog"
@@ -290,7 +289,7 @@ function PrettyDate(
   if (dateFormat) {
     formatted = dateFormat(date);
   } else {
-    formatted = date.toISOString().split('T')[0];
+    formatted = date.toISOString().split("T")[0];
   }
   return <time dateTime={date.toISOString()}>{formatted}</time>;
 }
