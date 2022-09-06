f1a5e67ffc99238548e2a958bb957227092184a0
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-09-06 13:39:51 -0700

    Bumped deps

--- deps.ts
+++ deps.ts
@@ -1,18 +1,18 @@
 // Copyright 2022 the Deno authors. All rights reserved. MIT license.
 
-export { serveDir } from "https://deno.land/std@0.153.0/http/file_server.ts";
-export { walk } from "https://deno.land/std@0.153.0/fs/walk.ts";
+export { serveDir } from "https://deno.land/std@0.154.0/http/file_server.ts";
+export { walk } from "https://deno.land/std@0.154.0/fs/walk.ts";
 export {
   dirname,
   fromFileUrl,
   join,
   relative,
-} from "https://deno.land/std@0.153.0/path/mod.ts";
+} from "https://deno.land/std@0.154.0/path/mod.ts";
 export {
   type ConnInfo,
   serve,
-} from "https://deno.land/std@0.153.0/http/mod.ts";
-export { extract as frontMatter } from "https://deno.land/std@0.153.0/encoding/front_matter.ts";
+} from "https://deno.land/std@0.154.0/http/mod.ts";
+export { extract as frontMatter } from "https://deno.land/std@0.154.0/encoding/front_matter.ts";
 
 export * as gfm from "https://deno.land/x/gfm@0.1.22/mod.ts";
 export {
@@ -32,7 +32,7 @@ export { Feed, type Item as FeedItem } from "https://esm.sh/feed@4.2.2";
 export { default as removeMarkdown } from "https://esm.sh/remove-markdown@0.5.0";
 
 // Add syntax highlighting support for C by default
-import "https://esm.sh/prismjs@1.28.0/components/prism-c?no-check";
+import "https://esm.sh/prismjs@1.29.0/components/prism-c?no-check";
 
 export { UnoCSS };
 export type UnoConfig = typeof UnoCSS extends (
