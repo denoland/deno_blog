05c0b3da372a637876091f75706afee848ac1892
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-01-30 18:28:40 -0800

    chore: bump deps

--- blog.tsx
+++ blog.tsx
@@ -24,6 +24,7 @@ import {
   serve,
   serveDir,
   UnoCSS,
+  ColorScheme,
   walk,
 } from "./deps.ts";
 import { Index, PostPage } from "./components.tsx";
@@ -101,6 +102,7 @@ function errorHandler(err: unknown) {
  */
 export default async function blog(settings?: BlogSettings) {
   html.use(UnoCSS(settings?.unocss)); // Load custom unocss module if provided
+  html.use(ColorScheme("auto"))
 
   const url = callsites()[1].getFileName()!;
   const blogState = await configureBlog(url, IS_DEV, settings);
@@ -310,7 +312,6 @@ export async function handler(
   }
 
   const sharedHtmlOptions: HtmlOptions = {
-    colorScheme: blogState.theme ?? "auto",
     lang: blogState.lang ?? "en",
     scripts: IS_DEV ? [{ src: "/hmr.js" }] : undefined,
     links: [
--- deps.ts
+++ deps.ts
@@ -1,28 +1,32 @@
 // Copyright 2022 the Deno authors. All rights reserved. MIT license.
 
-export { serveDir } from "https://deno.land/std@0.171.0/http/file_server.ts";
-export { walk } from "https://deno.land/std@0.171.0/fs/walk.ts";
+export { serveDir } from "https://deno.land/std@0.175.0/http/file_server.ts";
+export { walk } from "https://deno.land/std@0.175.0/fs/walk.ts";
 export {
   dirname,
   fromFileUrl,
   join,
   relative,
-} from "https://deno.land/std@0.171.0/path/mod.ts";
+} from "https://deno.land/std@0.175.0/path/mod.ts";
 export {
   type ConnInfo,
   serve,
-} from "https://deno.land/std@0.171.0/http/mod.ts";
-export { extract as frontMatter } from "https://deno.land/std@0.171.0/encoding/front_matter.ts";
+} from "https://deno.land/std@0.175.0/http/mod.ts";
+export { extract as frontMatter } from "https://deno.land/std@0.175.0/encoding/front_matter/any.ts";
 
-export * as gfm from "https://deno.land/x/gfm@0.1.27/mod.ts";
+export * as gfm from "https://deno.land/x/gfm@0.1.29/mod.ts";
 export {
   Fragment,
   h,
-  html,
+} from "https://deno.land/x/htm@0.1.3/mod.ts";
+export {
+  default as html,
   type HtmlOptions,
   type VNode,
-} from "https://deno.land/x/htm@0.0.10/mod.tsx";
-import { UnoCSS } from "https://deno.land/x/htm@0.0.10/plugins.ts";
+} from "https://deno.land/x/htm@0.1.3/html.tsx"
+import UnoCSS from "https://deno.land/x/htm@0.1.3/plugins/unocss.ts";
+import ColorScheme from "https://deno.land/x/htm@0.1.3/plugins/color-scheme.ts";
+
 export {
   createReporter,
   type Reporter as GaReporter,
@@ -34,7 +38,7 @@ export { default as removeMarkdown } from "https://esm.sh/remove-markdown@0.5.0"
 // Add syntax highlighting support for C by default
 import "https://esm.sh/prismjs@1.29.0/components/prism-c?no-check";
 
-export { UnoCSS };
+export { UnoCSS, ColorScheme };
 export type UnoConfig = typeof UnoCSS extends (
   arg: infer P | undefined,
 ) => unknown ? P
